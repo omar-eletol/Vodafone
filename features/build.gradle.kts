5f1ca90a3393c8977f00e6d3af85aad661fc5ca7
OmarEletol <omareletol@gmail.com>
2025-02-15 21:49:21 +0200

    -- refactor dependencies in the feature module

--- features/build.gradle.kts
+++ features/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -23,6 +24,14 @@ android {
             )
         }
     }
+    buildFeatures {
+        compose = true
+        buildConfig = true
+        viewBinding = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -34,10 +43,44 @@ android {
 
 dependencies {
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    api(project(":data"))
+    api(project(":core"))
+
+    api(libs.androidx.core.ktx)
+    api(libs.androidx.appcompat)
+    api(libs.material)
+    api(libs.androidx.security.crypto.ktx)
+    api(libs.androidx.activity.compose)
+    api(libs.androidx.lifecycle.runtime.ktx)
+    api(libs.androidx.lifecycle.runtime.compose)
+    api(libs.androidx.lifecycle.process)
+    api(libs.androidx.compose.bom)
+    api(libs.androidx.compose.ui.ui)
+    api(libs.androidx.compose.ui.ui.graphics)
+    api(libs.androidx.compose.ui.ui.tooling.preview)
+    api(libs.androidx.material)
+    api(libs.androidx.constraintlayout)
+    api(libs.androidx.material3.android)
+    // navigation / liveData / viewmodel
+    api(libs.androidx.lifecycle.livedata.ktx)
+    api(libs.androidx.lifecycle.viewmodel.ktx)
+    api(libs.androidx.navigation.fragment.ktx)
+
+    // swipe
+    api(libs.androidx.swiperefreshlayout)
+    api(libs.accompanist.swiperefresh)
+
+
+    // glide
+    api(libs.compose)
+
+    // Testing dependencies
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit.v121)
     androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation(libs.mockito.inline)
+    testImplementation(libs.mockito.kotlin)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.turbine)
+
 }
--- features/cityinput/build.gradle.kts
+++ features/cityinput/build.gradle.kts
@@ -43,50 +43,14 @@ android {
 }
 
 dependencies {
-    implementation(project(":data"))
-    implementation(project(":core"))
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.security.crypto.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.lifecycle.runtime.compose)
-    implementation(libs.androidx.lifecycle.process)
-    implementation(libs.androidx.compose.bom)
-    implementation(libs.androidx.compose.ui.ui)
-    implementation(libs.androidx.compose.ui.ui.graphics)
-    implementation(libs.androidx.compose.ui.ui.tooling.preview)
-    implementation(libs.androidx.material)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.material3.android)
-    // navigation / liveData / viewmodel
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    // swipe
-    implementation(libs.androidx.swiperefreshlayout)
-    implementation(libs.accompanist.swiperefresh)
-    implementation(libs.androidx.navigation.fragment.ktx)
-
-
+    implementation(project(":features"))
 
     // hilt
-    implementation(libs.hilt.android)
+    api(libs.hilt.android)
     kapt(libs.hilt.compiler)
     kapt(libs.androidx.hilt.compiler)
     compileOnly(libs.assisted.inject.annotations.dagger2)
     kapt(libs.assisted.inject.processor.dagger2)
-
-    // glide
-    implementation(libs.compose)
-
-    // Testing dependencies
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit.v121)
-    androidTestImplementation(libs.androidx.espresso.core)
-    testImplementation(libs.mockito.inline)
-    testImplementation(libs.mockito.kotlin)
-    testImplementation(libs.kotlinx.coroutines.test)
 
 }
